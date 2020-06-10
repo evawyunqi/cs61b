501e8bbf065c31ed2550d367656f1ff69d06cc09
Eva Wei <evawyunqi@berkeley.edu>
2020-06-09 19:23:22 -0700

    ---lab1 completed

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -18,15 +18,14 @@ public class LeapYear {
         if (year % 100 == 0) {
             if ( year % 400 == 0) {
                 return True;
-            } else {
-                return False;
-            }
+            } 
+            return False;
+    
         } else {
             if (year % 4 == 0) {
                 return True;
-            } else {
-                return False;
-            }
+            } 
+            return False;
         }
 
     }
